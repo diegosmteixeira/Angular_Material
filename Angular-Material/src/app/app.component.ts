e9af44cf1377f7bf0bd3dd5bb333f35728779431
Diego Teixeira <diegosmteixeira@gmail.com>
2023-04-26 18:30:51 -0300

    Fixed toolbar while scroll

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 64;
+export const SHADOW_LIMIT = 100;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public smallScreen = false;
+  public popText = false;
+  public applyShadow = false;
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+    fromEvent(content, 'scroll')
+      .pipe(
+        map(() => content.scrollTop )
+      )
+      .subscribe({
+        next: (value: number) => this.determineHeader(value)
+      })
+  }
+
+  determineHeader(scrollTop: number) {
+    this.popText = scrollTop >= TEXT_LIMIT;
+    this.applyShadow = scrollTop >= SHADOW_LIMIT;
+  }
   // ngAfterContentInit(): void {
   //   this.breakpointObserver.observe(['(max-width: 800px)'])
   //   .subscribe({
