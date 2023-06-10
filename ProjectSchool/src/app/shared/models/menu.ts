24131d3c002fb53583a28ebcd54b3e793c77e90f
Diego Teixeira <diegosmteixeira@gmail.com>
2023-06-09 23:10:15 -0300

    add flex-layout and models

---
+++ ProjectSchool/src/app/shared/models/menu.ts
@@ -0,0 +1,29 @@
+import { MenuItem } from "./menuItem";
+
+export const menuItems: MenuItem[] = [
+  {
+    link: '/dashboard',
+    icon: 'fa-chart-line',
+    label: 'Dashboard'
+  },
+  {
+    link: '/courses',
+    icon: 'fa-graduation-cap',
+    label: 'Courses'
+  },
+  {
+    link: '/teachers',
+    icon: 'fa-person-chalkboard',
+    label: 'Teachers'
+  },
+  {
+    link: '/students',
+    icon: 'fa-chalkboard-user',
+    label: 'Students'
+  },
+  {
+    link: '/support',
+    icon: 'fa-headset',
+    label: 'Support'
+  }
+]
