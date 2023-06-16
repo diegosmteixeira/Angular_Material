76fcc156af793bd7d01bf5a09b28874ab240120a
Diego Maggessi <diegosmteixeira@gmail.com>
2023-06-16 19:09:26 -0300

    add fontawesome

--- ProjectSchool/src/app/shared/components/toolbar-title/toolbar-title.component.ts
+++ ProjectSchool/src/app/shared/components/toolbar-title/toolbar-title.component.ts
@@ -7,7 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ToolbarTitleComponent implements OnInit {
   @Input() title: string = "";
-  @Input() icon: string = "";
+
+  // @Input() icon: string = "";
+  iconFa = '';
+  iconMat = '';
+
+  @Input() set icon(value: string) {
+    value.includes('fa-') ?
+    this.iconFa = `icon-space-fa ${value}` :
+    this.iconMat = value;
+  }
 
   constructor() { }
 
